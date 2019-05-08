7f17a321f94b53525d6d9d750f63354f9b260391
Nia Bert <nbert14@gmail.com>
2019-05-08 02:13:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -34,7 +34,7 @@ def reformat_languages(languages)
 
 new_hash = {}
 languages.each do |style, language| 
-  language.each do |name, attribute|
+  language.each do |name, attributes|
     new_hash[name] || = attributes
     new_hash[name][:style]=[]
     
